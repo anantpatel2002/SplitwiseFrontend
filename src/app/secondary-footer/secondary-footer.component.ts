05dc9af2788a22c68a184bd7851ae7f2cd5c40df
Anant Patel <anantpatel288@gmail.com>
2025-02-05 18:57:42 +0530

    secondary footer created

---
+++ src/app/secondary-footer/secondary-footer.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-secondary-footer',
+  imports: [],
+  templateUrl: './secondary-footer.component.html',
+  styleUrl: './secondary-footer.component.css'
+})
+export class SecondaryFooterComponent {
+
+}
