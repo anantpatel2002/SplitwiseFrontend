919f53765aa346af5c8a5ee1601c40b11230c358
Anant Patel <anantpatel288@gmail.com>
2025-02-06 15:18:26 +0530

    secondary footer made responsive and remove the timer onDestry lofe cycle hook

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,10 +2,11 @@ import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { HeaderComponent } from "./header/header.component";
 import { FooterComponent } from "./footer/footer.component";
+import { SecondaryFooterComponent } from './secondary-footer/secondary-footer.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, HeaderComponent, FooterComponent],
+  imports: [RouterOutlet, HeaderComponent, FooterComponent, SecondaryFooterComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
