eb369b8deaf11fdc7acb491bb76c165e24e79646
Ishant Yadav <2020501189.ishant@ug.sharda.ac.in>
2025-02-07 12:40:53 +0530

    main-page layout and dashboard

--- src/app/main-page/main-page.component.ts
+++ src/app/main-page/main-page.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { DashboardComponent } from "./dashboard/dashboard.component";
 
 @Component({
   selector: 'app-main-page',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, DashboardComponent],
   templateUrl: './main-page.component.html',
   styleUrl: './main-page.component.css'
 })
