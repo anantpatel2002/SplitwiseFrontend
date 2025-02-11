5655f260c2a4a982b653704776efe14c53a64403
Ishant Yadav <2020501189.ishant@ug.sharda.ac.in>
2025-02-11 12:01:41 +0530

    dashboard

--- src/app/main-page/main-page.component.ts
+++ src/app/main-page/main-page.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { GroupComponent } from "./group/group.component";
 import { DashboardComponent } from "./dashboard/dashboard.component";
 
 @Component({
   selector: 'app-main-page',
-  imports: [RouterOutlet, DashboardComponent],
+  imports: [RouterOutlet, GroupComponent, DashboardComponent],
   templateUrl: './main-page.component.html',
   styleUrl: './main-page.component.css'
 })
