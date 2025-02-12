6a6345c378c1610b036bd408b5c418d4140fd541
Anant Patel <anantpatel288@gmail.com>
2025-02-12 11:49:49 +0530

    minor changes

--- src/app/main-page/main-page.component.ts
+++ src/app/main-page/main-page.component.ts
@@ -1,12 +1,10 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-import { GroupComponent } from "./group/group.component";
-import { DashboardComponent } from "./dashboard/dashboard.component";
 import { LeftSidebarComponent } from "./left-sidebar/left-sidebar.component";
 
 @Component({
   selector: 'app-main-page',
-  imports: [RouterOutlet, GroupComponent, DashboardComponent, LeftSidebarComponent],
+  imports: [RouterOutlet, LeftSidebarComponent],
   templateUrl: './main-page.component.html',
   styleUrl: './main-page.component.css'
 })
