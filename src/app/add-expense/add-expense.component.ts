403bc81a2ebf4e059436ab61c6794f0013879d22
Ishant Yadav <2020501189.ishant@ug.sharda.ac.in>
2025-03-07 17:17:38 +0530

    choose currency

--- src/app/add-expense/add-expense.component.ts
+++ src/app/add-expense/add-expense.component.ts
@@ -1,84 +1,148 @@
-import { Component, EventEmitter, Output } from '@angular/core';
+import { Component, EventEmitter, OnInit, Output } from '@angular/core';
 import { ChoosePayerComponent } from "../choose-payer/choose-payer.component";
 import { animate, keyframes, state, style, transition, trigger } from '@angular/animations';
 import { DatePickerComponentComponent } from '../date-picker-component/date-picker-component.component';
 import { ChooseGroupComponent } from "../choose-group/choose-group.component";
 import { AddImageNotesComponent } from '../add-image-notes/add-image-notes.component';
 import { ChooseCategoryComponent } from '../choose-category/choose-category.component';
+import { ChooseCurrencyComponent } from "../choose-currency/choose-currency.component";
+import { ChooseSplitOptionsComponent } from "../choose-split-options/choose-split-options.component";
+import { FormControl, ReactiveFormsModule } from '@angular/forms';
+import { debounceTime, startWith } from 'rxjs';
 
 @Component({
   selector: 'app-add-expense',
-  imports: [ChoosePayerComponent, DatePickerComponentComponent, ChooseGroupComponent, AddImageNotesComponent, ChooseCategoryComponent],
+  imports: [
+    ReactiveFormsModule,
+    ChoosePayerComponent,
+    DatePickerComponentComponent,
+    ChooseGroupComponent,
+    AddImageNotesComponent,
+    ChooseCategoryComponent,
+    ChooseCurrencyComponent,
+    ChooseSplitOptionsComponent,
+  ],
   templateUrl: './add-expense.component.html',
   styleUrl: './add-expense.component.css',
   animations: [
     // Back div animation: keyframes for both forward and reverse transitions
     trigger('animateBack', [
-      state('start', style({
-        transform: 'translateX(-200px)',
-        opacity: 0
-      })),
+      state(
+        'start',
+        style({
+          transform: 'translateX(-200px)',
+          opacity: 0,
+        })
+      ),
       // "end" state: final position with the desired final style.
       // Here we’re keeping the final transform and opacity.
-      state('end', style({
-        transform: 'translateX(-140px) ',
-        opacity: 1
-      })),
+      state(
+        'end',
+        style({
+          transform: 'translateX(-140px) ',
+          opacity: 1,
+        })
+      ),
       // Forward animation: moves right while fading in then out
-      transition(":enter", animate('.2s ease-in-out', keyframes([
-        style({ transform: 'translateX(-200px)', opacity: 0, offset: 0 }),
-        style({ transform: 'translateX(-140px)', opacity: 1, offset: 1 })
-      ]))),
+      transition(
+        ':enter',
+        animate(
+          '.2s ease-in-out',
+          keyframes([
+            style({ transform: 'translateX(-200px)', opacity: 0, offset: 0 }),
+            style({ transform: 'translateX(-140px)', opacity: 1, offset: 1 }),
+          ])
+        )
+      ),
       // Reverse animation: moves from right to original position using reverse keyframes
-      transition(':leave', animate('.2s ease-in-out', keyframes([
-        style({ transform: 'translateX(-140px)', opacity: 1, offset: 0 }),
-        style({ transform: 'translateX(-200px)', opacity: 0, offset: 1 })
-      ])))
+      transition(
+        ':leave',
+        animate(
+          '.2s ease-in-out',
+          keyframes([
+            style({ transform: 'translateX(-140px)', opacity: 1, offset: 0 }),
+            style({ transform: 'translateX(-200px)', opacity: 0, offset: 1 }),
+          ])
+        )
+      ),
     ]),
     // Front div animation: simply slides left and back
     trigger('animateFront', [
-      state('start', style({
-        transform: 'translateX(0px)'
-      })),
-      state('end', style({
-        transform: 'translateX(-160px)'
-      })),
-      transition('start => end', animate('.2s ease-in-out', keyframes([
-        style({ transform: 'translateX(0px)', offset: 0 }),
-        style({ transform: 'translateX(-160px)', offset: 1 })
-      ]))),
-      transition('end => start', animate('.2s ease-in-out', keyframes([
-        style({ transform: 'translateX(-160px)', offset: 0 }),
-        style({ transform: 'translateX(0px)', offset: 1 })
-      ])))
-    ])
-  ]
+      state(
+        'start',
+        style({
+          transform: 'translateX(0px)',
+        })
+      ),
+      state(
+        'end',
+        style({
+          transform: 'translateX(-160px)',
+        })
+      ),
+      transition(
+        'start => end',
+        animate(
+          '.2s ease-in-out',
+          keyframes([
+            style({ transform: 'translateX(0px)', offset: 0 }),
+            style({ transform: 'translateX(-160px)', offset: 1 }),
+          ])
+        )
+      ),
+      transition(
+        'end => start',
+        animate(
+          '.2s ease-in-out',
+          keyframes([
+            style({ transform: 'translateX(-160px)', offset: 0 }),
+            style({ transform: 'translateX(0px)', offset: 1 }),
+          ])
+        )
+      ),
+    ]),
+  ],
 })
-export class AddExpenseComponent {
+export class AddExpenseComponent implements OnInit {
   @Output() expenseAdded = new EventEmitter();
 
   state = 'start';
-  showBackDiv = [false, false, false, false, false];
+  showBackDiv = [false, false, false, false, false, false];
+  searchControl = new FormControl('ishant');
+  
+  ngOnInit(): void {
+    this.searchControl.valueChanges
+      .pipe(
+        startWith(this.searchControl.value), // Emit the initial value immediately
+        debounceTime(300)
+      )
+      .subscribe({
+        next: (value) => {
+          console.log(value);
+        },
+        error: (err) => {
+          console.log(err);
+        },
+      });
+  }
 
   startAnimation(index: number) {
-    const result = this.showBackDiv.reduce((acc, value, index) =>{
-      if(value) {
+    const result = this.showBackDiv.reduce((acc, value, index) => {
+      if (value) {
         this.closeAnimation(index);
         console.log('current index' + index);
         acc.push(index);
       }
-      
+
       return acc;
     }, [] as number[]);
     console.log(result);
-    
-    if(!result.includes(index)) {
+
+    if (!result.includes(index)) {
       this.showBackDiv[index] = true;
-    this.state = 'end';
+      this.state = 'end';
     }
   }
-
-
 
   closeAnimation(index: number) {
     this.state = 'start';
---
+++ src/app/choose-currency/choose-currency.component.ts
@@ -0,0 +1,32 @@
+import { Component, EventEmitter, Output } from '@angular/core';
+
+@Component({
+  selector: 'app-choose-currency',
+  imports: [],
+  templateUrl: './choose-currency.component.html',
+  styleUrl: './choose-currency.component.css'
+})
+export class ChooseCurrencyComponent {
+  @Output() closeDiv = new EventEmitter();
+
+  CURRENCY_CODES: string[] = [
+    'AED', 'AFN', 'ALL', 'AMD', 'ANG', 'AOA', 'ARS', 'AUD', 'AWG', 'AZN',
+    'BAM', 'BBD', 'BDT', 'BGN', 'BHD', 'BIF', 'BMD', 'BND', 'BOB', 'BOV',
+    'BRL', 'BSD', 'BTN', 'BWP', 'BYN', 'BZD', 'CAD', 'CDF', 'CHE', 'CHF',
+    'CHW', 'CLF', 'CLP', 'CNY', 'COP', 'COU', 'CRC', 'CUC', 'CUP', 'CVE',
+    'CZK', 'DJF', 'DKK', 'DOP', 'DZD', 'EGP', 'ERN', 'ETB', 'EUR', 'FJD',
+    'FKP', 'FOK', 'GBP', 'GEL', 'GGP', 'GHS', 'GIP', 'GMD', 'GNF', 'GTQ',
+    'GYD', 'HKD', 'HNL', 'HRK', 'HTG', 'HUF', 'IDR', 'ILS', 'IMP', 'INR',
+    'IQD', 'IRR', 'ISK', 'JEP', 'JMD', 'JOD', 'JPY', 'KES', 'KGS', 'KHR',
+    'KID', 'KMF', 'KPW', 'KRW', 'KWD', 'KYD', 'KZT', 'LAK', 'LBP', 'LKR',
+    'LRD', 'LSL', 'LYD', 'MAD', 'MDL', 'MGA', 'MKD', 'MMK', 'MNT', 'MOP',
+    'MRU', 'MUR', 'MVR', 'MWK', 'MXN', 'MXV', 'MYR', 'MZN', 'NAD', 'NGN',
+    'NIO', 'NOK', 'NPR', 'NZD', 'OMR', 'PAB', 'PEN', 'PGK', 'PHP', 'PKR',
+    'PLN', 'PYG', 'QAR', 'RON', 'RSD', 'RUB', 'RWF', 'SAR', 'SBD', 'SCR',
+    'SDG', 'SEK', 'SGD', 'SHP', 'SLL', 'SLE', 'SOS', 'SRD', 'SSP', 'STN',
+    'SVC', 'SYP', 'SZL', 'THB', 'TJS', 'TMT', 'TND', 'TOP', 'TRY', 'TTD',
+    'TWD', 'TZS', 'UAH', 'UGX', 'USD', 'USN', 'UYI', 'UYU', 'UYW', 'UZS',
+    'VED', 'VES', 'VND', 'VUV', 'WST', 'XAF', 'XCD', 'XDR', 'XOF', 'XPF',
+    'YER', 'ZAR', 'ZMW', 'ZWL'
+  ];
+}
